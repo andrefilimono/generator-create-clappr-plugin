c0c80d40a3bf4a199d23efd02d5e5fd64b1b4811
Andrey Filimonow <andrefilimono@gmail.com>
2018-03-21 23:10:31 +0300

    Add plugin version property

--- generators/app/templates/main.js
+++ generators/app/templates/main.js
@@ -4,6 +4,10 @@ class <%= className %> extends <%= type %> {
   get name () {
     return '<%= name %>'
   }
+
+  static get version () {
+    return VERSION // eslint-disable-line
+  }
 }
 
 export default <%= className %>
